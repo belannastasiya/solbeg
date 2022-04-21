36836f878a9ad80bccbbc30ca83c154b034d0f61
belannastasiya <belannastasiya@mail.ru>
2022-04-21 15:27:17 +0300

    Successfull

--- cypress/integration/SC_auth_exp.js
+++ cypress/integration/SC_auth_exp.js
@@ -1,15 +1,41 @@
+const path = require('path');
+
 describe('Service Cannel opening', () => {
   it('open the site', () => {
     cy.visit('https://lt1login.servicechannel.com/account/logon?&returnUrl=https%3a%2f%2flt1.servicechannel.com%2fem%2fAccount%2fLogOn%3freturnUrl%3d%2fem%2fsummary');
   });
   it('enter username', () => {
     cy.get('#UserName')
-      .type('***');
+      .type(Cypress.env('username'));
   });
   it('enter password', () => {
-    cy.get('#Password') //privyay
-      .type('***');
+    cy.get('#Password')
+  
+      .type(Cypress.env('password'));
     cy.get('#LoginBtn').click();
-    cy.get('.ui-button').click();
-  })
-})
+  });
+
+  it('verifies download', () => {
+    cy.wait(3000);
+
+    cy.window().document().then(function (doc) {
+      doc.addEventListener('click', () => {
+        setTimeout(function () { doc.location.reload() }, 5000);
+      });
+      cy.get('.ui-button').click();
+    });
+
+    const fileName = 'EM_AssetSummaryReport_04-21-2022.xls';
+
+    const downloadsFolder = Cypress.config('downloadsFolder');
+    const downloadedFilename = path.join(downloadsFolder, fileName);
+
+    cy
+        .readFile(downloadedFilename, 'binary', {timeout: 15000})
+        .then((pdfDownloaded) => {
+            cy.fixture(fileName, 'binary', {timeout: 15000}).then((pdfFixture) => {
+                expect(pdfDownloaded).to.deep.equal(pdfFixture);
+            })
+        });
+});
+});
